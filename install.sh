583d4f5647a254d465ec7917cc13031ca90e325c
Max St-Onge <maxstonge@gmail.com>
2024-08-05 11:01:06 -0400

    Add git absorb

--- install.sh
+++ install.sh
@@ -35,6 +35,10 @@ if [ ! -d ~/fonts ]; then
     ~/fonts/install.sh
 fi
 
+if [ $SPIN ]; then
+  sudo apt-get install -y git-absorb
+fi
+
 # cp -R ~/dotfiles/.p10k.zsh ~/.p10k.zsh
 ln -fs ~/.oh-my-zsh/themes/powerlevel10k/powerlevel10k.zsh-theme ~/powerlevel10k/
 
